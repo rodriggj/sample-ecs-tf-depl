ae40bd3d390f65fc2262b0e9dc493ad4ea02c439
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2024-12-16 17:08:49 -0700

    adding nat igw and routes

--- 01-infrastructure/vpc.tf
+++ 01-infrastructure/vpc.tf
@@ -76,45 +76,80 @@ resource "aws_subnet" "private-subnet-3" {
 }
 
 resource "aws_route_table" "public_route_table" {
-    vpc_id = "${aws_vpc.production_vpc.id}"
-    tags = {
+    vpc_id  = "${aws_vpc.production_vpc.id}"
+    tags    = {
         Name = "Public-Route-Table"
     }
 }
 
 resource "aws_route_table" "private_route_table" {
-    vpc_id = "${aws_vpc.production_vpc.id}"
-    tags = {
+    vpc_id  = "${aws_vpc.production_vpc.id}"
+    tags    = {
         Name = "Private-Route-Table"
     }
 }
 
 resource "aws_route_table_association" "public_subnet_1_association" {
-  route_table_id = "${aws_route_table.public_route_table.id}"
-  subnet_id = "${aws_subnet.public-subnet-1.id}"
+  route_table_id    = "${aws_route_table.public_route_table.id}"
+  subnet_id         = "${aws_subnet.public-subnet-1.id}"
 }
 
 resource "aws_route_table_association" "public_subnet_2_association" {
-  route_table_id = "${aws_route_table.public_route_table.id}"
-  subnet_id = "${aws_subnet.public-subnet-2.id}"
+  route_table_id    = "${aws_route_table.public_route_table.id}"
+  subnet_id         = "${aws_subnet.public-subnet-2.id}"
 }
 
 resource "aws_route_table_association" "public_subnet_3_association" {
-  route_table_id = "${aws_route_table.public_route_table.id}"
-  subnet_id = "${aws_subnet.public-subnet-3.id}"
+  route_table_id    = "${aws_route_table.public_route_table.id}"
+  subnet_id         = "${aws_subnet.public-subnet-3.id}"
 }
 
 resource "aws_route_table_association" "private_subnet_1_association" {
-  route_table_id = "${aws_route_table.private_route_table.id}"
-  subnet_id = "${aws_subnet.private-subnet-1.id}"
+  route_table_id    = "${aws_route_table.private_route_table.id}"
+  subnet_id         = "${aws_subnet.private-subnet-1.id}"
 }
 
 resource "aws_route_table_association" "private_subnet_2_association" {
-  route_table_id = "${aws_route_table.private_route_table.id}"
-  subnet_id = "${aws_subnet.private-subnet-2.id}"
+  route_table_id    = "${aws_route_table.private_route_table.id}"
+  subnet_id         = "${aws_subnet.private-subnet-2.id}"
 }
 
 resource "aws_route_table_association" "private_subnet_3_association" {
-  route_table_id = "${aws_route_table.private_route_table.id}"
-  subnet_id = "${aws_subnet.private-subnet-3.id}"
+  route_table_id    = "${aws_route_table.private_route_table.id}"
+  subnet_id         = "${aws_subnet.private-subnet-3.id}"
+}
+
+resource "aws_eip" "elastic_ip_for_nat_gw" {
+  associate_with_private_ip = "10.0.0.5"
+  tags                      = {
+    Nmme = "Production-EIP"
+  }
+} 
+
+resource "aws_nat_gateway" "nat_gw" {
+    allocation_id   = "${aws_eip.elastic-ip-for-nat-gw.id}"
+    subnet_id       = "${aws_subnet.public-subnet-1.id}"
+    tags            = {
+      Name = "Production-NAT-Gtwy"
+    }
+    depends_on = [ "aws_eip.elastic_ip_for_nat_gw" ]
+}
+
+resource "aws_route" "nat_gw_route" {
+    route_table_id          = "${aws_route_table.private_route_table.id}"
+    nat_gateway_id          = "${aws_nat_gateway.nat_gw.id}"
+    destination_cidr_block  = "0.0.0.0/0"
+}
+
+resource "aws_internet_gateway" "production_igw" {
+    vpc_id  = "${aws_vpc.production_vpc.id}"
+    tags    = {
+      Name = "Production-IGW"
+    }
+}
+
+resource "aws_route" "public_igw_route" {
+    route_table_id          = "${aws_route_table.public_route_table.id}"
+    gateway_id              = "${aws_internet_gateway.production_igw.id}" 
+    destination_cidr_block  = "0.0.0.0/0"
 }
